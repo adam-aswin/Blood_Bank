0910efc69bf877b61a692eab5576a8be6ec91a27
adam-aswin <aswinsachu2118@gmail.com>
2024-11-04 17:58:46 +0530

    updated

--- blood_bank/lib/homepage.dart
+++ blood_bank/lib/homepage.dart
@@ -14,7 +14,7 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldkey,
-      backgroundColor: Colors.white,
+      backgroundColor: const Color.fromARGB(246, 255, 255, 255),
       appBar: AppBar(
         iconTheme: IconThemeData(),
         leading: IconButton(
@@ -43,8 +43,9 @@ class _HomepageState extends State<Homepage> {
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: ListView(
-          padding: EdgeInsets.only(left: 15, right: 15, top: 10),
+        padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               height: 50,
@@ -54,31 +55,198 @@ class _HomepageState extends State<Homepage> {
                 color: Colors.white,
                 boxShadow: [
                   BoxShadow(
-                    blurRadius: 8,
+                    blurRadius: 5,
                     spreadRadius: 0,
-                    color: const Color.fromARGB(255, 212, 212, 212),
+                    color: const Color.fromARGB(22, 0, 0, 0),
                     offset: Offset(5, 5),
                   ),
                   BoxShadow(
-                    blurRadius: 8,
+                    blurRadius: 5,
                     spreadRadius: 0,
-                    color: const Color.fromARGB(255, 223, 223, 223),
+                    color: const Color.fromARGB(17, 0, 0, 0),
                     offset: Offset(-5, -5),
                   ),
                 ],
               ),
               child: TextField(
                 controller: _search,
+                cursorColor: Colors.black54,
                 decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide.none,
+                  suffixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black54,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                  hintText: "Search...",
+                  hintStyle: TextStyle(color: Colors.black54),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 280,
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 5,
+                    spreadRadius: 0,
+                    color: const Color.fromARGB(22, 0, 0, 0),
+                    offset: Offset(5, 5),
+                  ),
+                  BoxShadow(
+                    blurRadius: 5,
+                    spreadRadius: 0,
+                    color: const Color.fromARGB(17, 0, 0, 0),
+                    offset: Offset(-5, -5),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Blood Type",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
+                  ),
+                  Text(
+                    "Availablity",
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Container(width: 32, child: Text("O+")),
+                            Container(
+                              width: MediaQuery.of(context).size.width * .7,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.horizontal(
+                                      right: Radius.circular(5))),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(width: 32, child: Text("AB+")),
+                            Container(
+                              width: MediaQuery.of(context).size.width * .6,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.horizontal(
+                                      right: Radius.circular(5))),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(width: 32, child: Text("A-")),
+                            Container(
+                              width: MediaQuery.of(context).size.width * .5,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.horizontal(
+                                      right: Radius.circular(5))),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(width: 32, child: Text("O-")),
+                            Container(
+                              width: MediaQuery.of(context).size.width * .4,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.horizontal(
+                                      right: Radius.circular(5))),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(width: 32, child: Text("B-")),
+                            Container(
+                              width: MediaQuery.of(context).size.width * .6,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.horizontal(
+                                      right: Radius.circular(5))),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(width: 32, child: Text("A+")),
+                            Container(
+                              width: MediaQuery.of(context).size.width * .7,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.horizontal(
+                                      right: Radius.circular(5))),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(width: 32, child: Text("AB-")),
+                            Container(
+                              width: MediaQuery.of(context).size.width * .4,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.horizontal(
+                                      right: Radius.circular(5))),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              width: 32,
+                              child: Text("B+"),
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width * .5,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.horizontal(
+                                      right: Radius.circular(5))),
+                            )
+                          ],
+                        ),
+                      ],
                     ),
-                    hintText: "Search...",
-                    hintStyle: TextStyle(color: Colors.black54)),
+                  ),
+                ],
               ),
             )
           ],
